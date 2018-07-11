62b346465850169691f33aa28872e42823407a70
gianlucapasqua <30430770+gianlucapasqua@users.noreply.github.com>
2018-07-11 10:53:57 +0200

    update files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-web: node index.js
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
