38caec011e0076453855a545b19a7dcb082ee848
gianlucapasqua <30430770+gianlucapasqua@users.noreply.github.com>
2018-07-11 11:24:10 +0200

    add default response for /teams

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
-
+app.all('/teams', function(request, response){
+    response.json("lista teams");
+});
