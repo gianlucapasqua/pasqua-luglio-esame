7178851a9048306b24829b3bb7264c23e168691e
gianlucapasqua <30430770+gianlucapasqua@users.noreply.github.com>
2018-07-11 11:41:53 +0200

    add arrays for teams and matches

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
+teams = [];
+matches = [];
+//to do: post and put to fill the 2 arrays
 app.all('/', function(request, response){
     response.json("/teams per la lista dei teams, /matches per la lista dei match");
 });
 app.all('/teams', function(request, response){
-    response.json("lista teams");
+    response.json(teams);
 });
 app.all('/matches', function(request, response){
-    response.json("lista matches");
+    response.json(matches);
 });
