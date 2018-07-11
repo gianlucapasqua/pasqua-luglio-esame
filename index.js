2c57f9a0b5bb1f6a4bfa1dc5b51916b26d19b45a
gianlucapasqua <30430770+gianlucapasqua@users.noreply.github.com>
2018-07-11 11:34:14 +0200

    update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+app.all('/', function(request, response){
+    response.json("/teams per la lista dei teams, /matches per la lista dei match");
+});
 app.all('/teams', function(request, response){
     response.json("lista teams");
+});
+app.all('/matches', function(request, response){
+    response.json("lista matches");
 });
